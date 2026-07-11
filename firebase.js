45817b816d24df1cb7827510088cbfb3f7a0ef90
bkbashir <bashirahmed0052@gmail.com>
2026-07-11 18:42:57 +0600

    Refactor Firebase imports and exports for clarity

--- firebase.js
+++ firebase.js
@@ -1,8 +1,32 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+// ===============================
+// Friendsbook V4
+// firebase.js
+// ===============================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+
+getFirestore
+
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+
+getStorage
+
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBRad-Z7zxRRnvy17nRXEh7ZG4hu6fluZ4",
   authDomain: "friendsbook-4a40c.firebaseapp.com",
@@ -12,10 +36,25 @@ const firebaseConfig = {
   appId: "1:1000346329473:web:9bd69019e2b09f971e8880"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+
 const db = getFirestore(app);
+
 const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export {
+
+app,
+
+auth,
+
+db,
+
+storage
+
+};
