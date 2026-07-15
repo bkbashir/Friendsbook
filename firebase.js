ab5f74767c1e75a63ccfa035bbb48c594620e42b
bkbashir <bashirahmed0052@gmail.com>
2026-07-15 10:39:46 +0600

    Add firbase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,70 @@
+// ======================================
+// Friendsbook Mobile V1
+// firebase.js
+// ======================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 
+// ======================================
+// Firebase Config
+// ======================================
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyBRad-Z7zxRRnvy17nRXEh7ZG4hu6fluZ4",
+
   authDomain: "friendsbook-4a40c.firebaseapp.com",
+
   projectId: "friendsbook-4a40c",
+
   storageBucket: "friendsbook-4a40c.firebasestorage.app",
+
   messagingSenderId: "1000346329473",
+
   appId: "1:1000346329473:web:9bd69019e2b09f971e8880"
+
 };
+
+// ======================================
+// Initialize Firebase
+// ======================================
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export { app };
+// ======================================
+// Services
+// ======================================
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+// ======================================
+// Export
+// ======================================
+
+export {
+
+app,
+
+auth,
+
+db,
+
+storage
+
+};
+
+// ======================================
+// Friendsbook
+// ======================================
+
+console.log("Friendsbook Firebase Connected");
