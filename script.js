45ece73bf9ab05724278123d989fb7c7379c7598
bkbashir <bashirahmed0052@gmail.com>
2026-07-16 18:34:02 +0600

    Add script.js

--- script.js
+++ script.js
@@ -62,3 +62,72 @@ window.onload=()=>{
 console.log("Friendsbook 2026 Loaded");
 
 };
+// =========================
+// Friendsbook 2026
+// Part 2
+// Create Post + Stories
+// =========================
+
+// Create Post
+
+const postInput=document.querySelector(".postTop input");
+
+const livePostBtn=document.getElementById("livePostBtn");
+
+const photoPostBtn=document.getElementById("photoPostBtn");
+
+const feelingPostBtn=document.getElementById("feelingPostBtn");
+
+// Story
+
+const createStory=document.querySelector(".createStory");
+
+// =========================
+// Create Post
+// =========================
+
+postInput.addEventListener("click",()=>{
+
+console.log("Open Create Post");
+
+});
+
+livePostBtn.addEventListener("click",()=>{
+
+console.log("Live Video");
+
+});
+
+photoPostBtn.addEventListener("click",()=>{
+
+console.log("Photo Upload");
+
+});
+
+feelingPostBtn.addEventListener("click",()=>{
+
+console.log("Feeling Activity");
+
+});
+
+// =========================
+// Story
+// =========================
+
+createStory.addEventListener("click",()=>{
+
+console.log("Create Story");
+
+});
+
+// Story Click
+
+document.querySelectorAll(".story").forEach(story=>{
+
+story.addEventListener("click",()=>{
+
+console.log("Open Story");
+
+});
+
+});
